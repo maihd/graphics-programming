9a67d339a3ff37a24852fff4e8355b709343f049
maihd <maihd@gmail.com>
2025-08-08 23:15:24 +0400

    update windows build

--- c/webgpu/build_clang.bat
+++ c/webgpu/build_clang.bat
@@ -1,5 +1,6 @@
 @echo off
 
 set INCLUDE_DIRS=-I3rd_party/wgpu-native/include -I../vectormath/include
+set LIBRARY_DIRS=-L3rd_party/wgpu-native/lib/win64
 
-clang src\main.c -o webgpu_demos.exe %INCLUDE_DIRS% 3rd_party/wgpu-native/lib/wgpu_native.lib -std=c11
+clang src\main.c -o webgpu_demos.exe %INCLUDE_DIRS% -lwgpu_native -std=c11
