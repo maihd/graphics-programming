a3fc16917eb470d04d60d7c742df5433909cbf9e
ZenFox <maihd.dev@gmail.com>
2025-08-16 10:39:36 +0400

    build for windows

--- c/build_deps.bat
+++ c/build_deps.bat
@@ -1,9 +1,11 @@
 @echo off
 
 set SDL3_FOLDER=SDL-release-3.2.20
+set SDL3_SHADERCROSS_FOLDER=SDL_shadercross
 
 set CMAKE_COMPILER=-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_STANDARD=11 -DCMAKE_CXX_STANDARD=20
 set CMAKE_LIBRARY_TARGET=-DBUILD_SHARED_LIBS=OFF
+set CMAKE_INSTALL_PREFIX=-DCMAKE_INSTALL_PREFIX=../../installed_deps
 
 :: Building SDL3
 echo Building SDL3...
@@ -15,9 +17,11 @@ if exist build (
 
 mkdir build && cd build
 
-cmake .. -GNinja %CMAKE_COMPILER% %CMAKE_LIBRARY_TARGET%
+cmake .. -GNinja %CMAKE_COMPILER% %CMAKE_LIBRARY_TARGET% %CMAKE_INSTALL_PREFIX%
 
 cmake --build .
+
+cmake --install .
 
 cd ..
 
@@ -30,5 +34,55 @@ if not exist prebuilt\win64 (
 )
 
 copy build\SDL3-static.lib prebuilt\win64\SDL3-static.lib
+
+popd
+
+pushd %SDL3_SHADERCROSS_FOLDER%
+
+pushd external\spirv-cross
+
+if exist build (
+    rmdir build /S /Q
+)
+
+mkdir build && cd build
+
+cmake .. -GNinja %CMAKE_COMPILER% %CMAKE_LIBRARY_TARGET% -DCMAKE_INSTALL_PREFIX=../../../../installed_deps
+
+cmake --build .
+
+cmake --install .
+
+cd ..
+
+popd
+
+if exist build (
+    rmdir build /S /Q
+)
+
+mkdir build && cd build
+
+cmake .. -GNinja %CMAKE_COMPILER% %CMAKE_LIBRARY_TARGET% %CMAKE_INSTALL_PREFIX% ^
+    -DSDLSHADERCROSS_SPIRVCROSS_SHARED=OFF ^
+    -DSDLSHADERCROSS_DXC=OFF ^
+    -DSDLSHADERCROSS_VENDORED=ON
+
+cmake --build .
+
+cmake --install .
+
+cd ..
+
+if not exist prebuilt (
+    mkdir prebuilt
+)
+
+if not exist prebuilt\win64 (
+    mkdir prebuilt\win64
+)
+
+copy build\SDL3_shadercross.lib prebuilt\win64\SDL3_shadercross.lib
+copy external\SPIRV-Cross\build\spirv-cross-*.lib prebuilt\win64\spirv-cross-cd.lib
 
 popd
--- c/sdl3_gpu_examples/build_clang.bat
+++ c/sdl3_gpu_examples/build_clang.bat
@@ -1,11 +1,22 @@
 @echo off
 
-set SDL3_FOLDER=../SDL-Release-3.2.20
+set SDL3_FOLDER=../SDL-release-3.2.20
+set SDL3_SHADERCROSS_FOLDER=../SDL_shadercross
+
+set PREBUILT_FOLDER=prebuilt/win64
 
 set WIN32_LIBS=^
     -lKernel32 -lUser32 -lGdi32 -lShell32 -lWinmm -lOle32 -lVersion ^
     -lCfgMgr32 -lImm32 -lSetupapi -lAdvapi32 -lOleAut32 ^
-    -lMsvcrt
+    -lMsvcrtd
 
+set SHADERCROSS_LIBS=^
+    -I%SDL3_SHADERCROSS_FOLDER%/include                             ^
+    -L%SDL3_SHADERCROSS_FOLDER%/%PREBUILT_FOLDER%                   ^
+    -lSDL3_shadercross                                              ^
+    -lspirv-cross-cored -lspirv-cross-cd  -lspirv-cross-cppd        ^
+    -lspirv-cross-msld -lspirv-cross-hlsld -lspirv-cross-glsld      ^
+    -lspirv-cross-reflectd -lspirv-cross-utild                      ^
+    -lstdc++
 
-clang main.c -I%SDL3_FOLDER%/include %SDL3_FOLDER%/prebuilt/win64/SDL3-static.lib -D_CRT_SECURE_NO_WARNINGS %WIN32_LIBS%
+clang main.c -I%SDL3_FOLDER%/include %SDL3_FOLDER%/%PREBUILT_FOLDER%/SDL3-static.lib -D_CRT_SECURE_NO_WARNINGS %WIN32_LIBS% %SHADERCROSS_LIBS%
